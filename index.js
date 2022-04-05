7dd4b38a15016b36c34eeb5bf3369f5aa015019a
Allison Bohannon <akboh.github@gmail.com>
2022-04-04 20:21:40 -0700

    commit tree f793d5b3d384715c10c429333503649c639aeb38

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 1; 
+const num2 = 62;
+const multiply = num1 * num2; 
+const random = Math.ceil(Math.random()*1000); 
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4; 
+const max = Math.max(10, 15, 20); 
